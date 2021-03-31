33fef87d5014dc086f0be4bedd7246801152cb5b
RudeSploit <44660255+zakariasantoso@users.noreply.github.com>
2021-03-31 16:39:37 +0700

    Add files via upload

---
+++ language/nad.js
@@ -0,0 +1,121 @@
+exports.wait = () => {
+	return`*「❗」WAIT KAK*`
+}
+
+exports.succes = () => {
+	return`*「 SUKSES 」*`
+}
+
+exports.lvlon = () => {
+	return`*「❗」LEVELING ENABLE*`
+}
+
+exports.lvloff = () => {
+	return`*「❗」LEVELING DISABLE*`
+}
+
+exports.lvlnul = () => {
+	return`*「❗」LEVELMU MASIH KOSONG*`
+}
+
+exports.lvlnoon = () => {
+	return`*「❗」LEVEL DI GRUB BELUM DI AKTIFKAN*`
+}
+
+exports.noregis = () => {
+	return`*「❗」BELUM VERIFIKASI「❗」*\n\nketik : @verify`
+}
+
+exports.baned = () => {
+	return`*「❗」SORRY SORRY AJA NIH BRO, TAPI KAU SUDAH KU BANNED YAHAHAHA HAYUUU :V*`
+}
+
+exports.premium = (prefix) => {
+	return`Lu Siapa? Fitur ini khusus user premium!
+Upgrade ke premium dulu bosku
+Jika minat hubungi owner ku
+Ketik : ${prefix}owner`
+}
+
+exports.rediregis = () => {
+	return`*Kamu sudah melakukan verify -_-*`
+}
+
+exports.stikga = () => {
+	return`*「 GAGAL 」Coba reply/tag ulang kak*`
+}
+
+exports.linkga = () => {
+	return`*「❗」maaf linknya tidak valid kak*`
+}
+
+exports.groupo = () => {
+	return`*「❗」GROUP ONLY*`
+}
+
+exports.ownerb = () => {
+	return`*「❗」OWNER BOT ONLY*`
+}
+
+exports.ownerg = () => {
+	return`*「❗」OWNER GROUP ONLY*`
+}
+
+exports.admin = () => {
+	return`*「❗」ADMIN GROUP ONLY*`
+}
+
+exports.badmin = () => {
+	return`*「❗」BOT HARUS JADI ADMIN*`
+}
+
+exports.bug = () => {
+	return`*Masalah telah di laporkan ke owner BOT, laporan palsu/main2 tidak akan ditanggapi*`
+}
+
+exports.wrongf = () => {
+	return`*「🗿」Teks nya mana kak?*`
+}
+
+exports.clears = () => {
+	return`*Sukses bosku*`
+}
+
+exports.levelup = (pushname, sender, getLevelingXp,  getLevel, getLevelingLevel) => {
+	return`
+*「 SELAMAT 」*
+\`\`\`➸ *Nama* : ${pushname}\`\`\`
+\`\`\`➸ *Nomor* : wa.me/${sender.split("@")[0]}\`\`\`
+\`\`\`➸ *Xp* : ${getLevelingXp(sender)}\`\`\`
+\`\`\`➸ *Level* : ${getLevel} ➸ ${getLevelingLevel(sender)}\`\`\`
+`}
+ 
+exports.limitend = (pushname, prefix) => {
+	return`*maaf ${pushname} limit hari ini habis*
+*Limit di reset setiap jam 24:00*
+
+\`\`\`Upgrade Premium Bebas Limit\`\`\`
+\`\`\`➸ 20K : 1 Bulan\`\`\`
+\`\`\`Jika berminat silahkan hubungi owner\`\`\`
+\`\`\`Kirim perintah : ${prefix}owner\`\`\`
+`
+}
+
+exports.limitcount = (isPrem, limitCounts) => {
+	return`
+*「 LIMIT COUNT 」*
+sisa limit anda : ${isPrem ? '9999' : `${limitCounts}`}
+
+Upgrade premium bosku, biar bebas gunain bot`
+}
+
+
+exports.uangkau = (pushname, sender, uangkau) => {
+	return`┏━━━━━━━♡ *ATM* ♡━━━━━━━┓
+┃╭───────────────────
+┃│➸ NAMA : ${pushname}
+┃│➸ NOMOR : ${sender.split("@")[0]}
+┃│➸ UANG : ${uangkau}
+┃╰───────────────────
+┗━━━━━━━━━━━━━━━━━━━━┛`
+}
